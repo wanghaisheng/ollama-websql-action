b7afce724093f3ee94490b67aaa8fa126e62f868
Samuel Colvin <s@muelcolvin.com>
2024-12-08 21:50:10 +0000

    add ollama to path

--- install-ollama.sh
+++ install-ollama.sh
@@ -3,7 +3,14 @@
 set -e
 set -x
 
+# from https://github.com/ollama/ollama/blob/v0.5.1/scripts/install.sh#L32-L37
 ARCH=$(uname -m)
+case "$ARCH" in
+    x86_64) ARCH="amd64" ;;
+    aarch64|arm64) ARCH="arm64" ;;
+    *) error "Unsupported architecture: $ARCH" ;;
+esac
+
 curl -L https://ollama.com/download/ollama-linux-${ARCH}.tgz -o ollama.tgz
 mkdir /tmp/ollama-run
 tar -C /tmp/ollama-run -xzf ollama.tgz
--- start-ollama.sh
+++ start-ollama.sh
@@ -8,4 +8,6 @@ sudo cp ollama.service /etc/systemd/system/ollama.service
 sudo systemctl daemon-reload
 sudo systemctl start ollama
 
+echo "/tmp/ollama-run/bin/" >> $GITHUB_PATH
+
 sleep 2
