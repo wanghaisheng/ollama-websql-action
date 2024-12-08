d1286bc125b393d21ea6f27dd2da66e135b17bdb
Samuel Colvin <s@muelcolvin.com>
2024-12-08 21:39:25 +0000

    try using a temporary directory for install

--- install-ollama.sh
+++ install-ollama.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 curl -L https://ollama.com/download/ollama-linux-amd64.tgz -o ollama-linux-amd64.tgz
-mkdir /ollama-run
-tar -C /ollama-run -xzf ollama-linux-amd64.tgz
+mkdir /tmp/ollama-run
+tar -C /tmp/ollama-run -xzf ollama-linux-amd64.tgz
 
-/ollama-run/bin/ollama --version
+/tmp/ollama-run/bin/ollama --version
